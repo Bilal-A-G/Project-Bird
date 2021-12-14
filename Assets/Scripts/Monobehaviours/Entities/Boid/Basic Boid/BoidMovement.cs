864b33ea37de474b117e0a5f78ebff30bec64bbc
Bilal Abdul Gaffoor <bilalabdulg@gmail.com>
2021-12-14 17:17:27 -0500

    Made the boids a prefab and coded a rudimentary movement system for them

--- Assets/Scripts/Monobehaviours/Entities/Boid/Basic Boid/BoidMovement.cs
+++ Assets/Scripts/Monobehaviours/Entities/Boid/Basic Boid/BoidMovement.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class BoidMovement : MonoBehaviour, IMovable
 {
-    public void Look(Vector2 lookVector)
-    {
-        throw new System.NotImplementedException();
-    }
+    public GameObject player;
+    public float speed;
+
+    CharacterController characterController;
+
+    public float boidDistance;
+    public float detectionDistance;
+
+    Vector3 step;
 
     public void Move(IMovable.MoveDirections moveDirections)
     {
         if(moveDirections == IMovable.MoveDirections.Any)
         {
+            if(player != null && (player.transform.position - transform.position).magnitude <= detectionDistance)
+            {
+                step = (player.transform.position - transform.position).normalized * speed * Time.deltaTime;
+            }
+            else
+            {
+                step = Vector3.zero;
+            }
 
+            characterController.Move(new Vector3(step.x, -9.8f, step.z));
         }
     }
 
-    public void Move(Vector2 moveAxis) => throw new System.NotImplementedException();
-
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        
+
     }
+
+    public void Look(Vector2 lookVector) => throw new System.NotImplementedException();
+    public void Move(Vector2 moveAxis) => throw new System.NotImplementedException();
 }
